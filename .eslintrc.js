f95c437e423ef8a566e0dc45d585954f029e485d
fangornn <fanghornn@gmail.com>
2018-10-08 22:23:30 +0200

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js"] }],
+    },
+    "env": {
+        "browser": true,
+    }
+};
