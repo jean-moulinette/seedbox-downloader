729bf0b71294516561fe8741dd780c4bf4d0d3a6
fangornn <fanghornn@gmail.com>
2019-05-30 20:41:24 +0200

    - Feature: The user can now folders directly as zip - Bump npm version

--- client/src/ui/blocks/directory-card/component.js
+++ client/src/ui/blocks/directory-card/component.js
@@ -1,41 +1,14 @@
 import * as React from 'react';
 
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 import { APP_FONT_STYLES } from 'ui/helpers/typography';
 import { APP_SCALES } from 'ui/helpers/scales';
 import FolderFlatIcon from 'icons/folder-flat/component';
 import PropTypes from 'prop-types';
 
-const MenuOptionsHoverIn = keyframes`
-  0% {
-    /* visibility: hidden;*/
-    transform: translate(35px, 0px);
-  }
-  100% {
-    transform: translate(-25px, 0px);
-    /* visibility: visible; */
-  }
-`;
-
-const MenuOptionsHoverOut = keyframes`
-  0% {
-    /* visibility: visible */
-    transform: translate(-25px, 0px);
-  }
-  100% {
-    /* visibility: hidden; */
-    transform: translate(35px, 0px);
-  }
-`;
-
 const Container = styled.div`
   position: relative;
-  &:hover {
-    .inner-menu-container {
-      animation: ${MenuOptionsHoverIn} 1s ease-out
-    }
-  }
 `;
 
 const FolderAnchor = styled.a`
@@ -55,8 +28,25 @@ const FolderAnchor = styled.a`
   transition:  box-shadow 0.3s cubic-bezier(.25,.8,.25,1);
   border-radius: 3px;
 
+  /* Little hack to target the button instead of it's container */
+  &+div.inner-menu-container>div {
+    transform: rotate(180deg);
+    opacity: 0;
+    transition: all 1s cubic-bezier(.25,.8,.25,1);
+    transition-property: transform, opacity;
+  }
+
   &:hover {
     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+    &+div {
+      transform: translateY(-60px);
+
+      &.inner-menu-container>div {
+        opacity: 1;
+        transform: rotate(0deg);
+      }
+    }
   }
 
   &:focus {
@@ -66,12 +56,21 @@ const FolderAnchor = styled.a`
 
 const InnerMenuContainer = styled.div`
   position: absolute;
-  top: calc(${APP_SCALES.WINDOW_CONTENT.DIRECTORY_CARD_HEIGHT} / 2);
   right: ${APP_SCALES.WINDOW_CONTENT.DIRECTORY_CARD_INNER_MENU_RIGHT_MARGIN};
   height: ${APP_SCALES.WINDOW_CONTENT.DIRECTORY_CARD_INNER_MENU_ICON_SIZE};
   width: ${APP_SCALES.WINDOW_CONTENT.DIRECTORY_CARD_INNER_MENU_ICON_SIZE};
   border-radius: 100%;
-  animation: ${MenuOptionsHoverOut} 1s ease-in
+  transition: transform 0.8s cubic-bezier(.25,.8,.25,1);
+
+  /* Little hack to keep the button container on the Card even while hovering it */
+  &:hover {
+    transform: translateY(-60px) scale(1.1);
+
+    &>div {
+      transform: rotate(0deg) !important;
+      opacity: 1 !important;
+    }
+  }
 `;
 
 const Icon = styled(FolderFlatIcon)`
