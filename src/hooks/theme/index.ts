92b93c746eb684cb7678126b41014bf218825936
fangornn <fanghornn@gmail.com>
2021-07-25 13:58:16 +0200

    typescript conversion

--- src/hooks/theme/index.ts
+++ src/hooks/theme/index.ts
@@ -1,15 +1,18 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from 'styled-components';
-import { LightThemeSymbol, DarkThemeSymbol, themes } from 'ui/helpers/colors';
+import { DarkThemeSymbol, LightThemeSymbol, themes } from 'ui/helpers/colors';
+import type { ThemeSymbol } from 'ui/helpers/colors';
 
-function getThemeSymbolByThemeObject(themeObject) {
+type ThemeObject = typeof themes[ThemeSymbol]
+
+function getThemeSymbolByThemeObject(themeObject: ThemeObject) {
   return themeObject === themes[LightThemeSymbol]
     ? LightThemeSymbol
     : DarkThemeSymbol;
 }
 
 export default function useTheme() {
-  const themeObject = useContext(ThemeContext);
+  const themeObject = useContext<ThemeObject>(ThemeContext);
   const themeSymbol = getThemeSymbolByThemeObject(themeObject);
   const [currentTheme, setCurrentTheme] = useState(themeSymbol);
 
