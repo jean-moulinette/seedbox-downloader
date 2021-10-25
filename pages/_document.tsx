91eb4e3b07d29440dbcabbb97a066ae1b7456a64
fangornn <fanghornn@gmail.com>
2021-10-26 00:00:07 +0200

    add leading slashes to document resources

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,14 +26,14 @@ class MyDocument extends Document {
             content={APP_COLORS.PRIMARY_COLOR}
           />
           <link rel="icon" href="favicon.ico" />
-          <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
-          <link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon-180x180.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
 
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
